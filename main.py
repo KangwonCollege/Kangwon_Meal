01cd1d1229170898834fbefaf4139d6b501b315b
gunyu1019 <gunyu1019@gmail.com>
2023-11-12 02:17:13 +0900

    [Fix] Use only one client

--- main.py
+++ main.py
@@ -16,13 +16,3 @@ task_image_supporter = ImportSupporter(background_scheduler)
 
 view_image_supporter.load_modules('routers', directory)
 task_image_supporter.load_modules('tasks', directory, after_loaded=lambda: background_scheduler.start())
-
-
-host = "127.0.0.1"
-port = 8000
-app_name = "routers.router:app"
-
-if __name__ == "__main__":
-    config = uvicorn.Config(app=app_name, host=host, port=port, reload=True)
-    server = uvicorn.Server(config=config)
-    server.run()
