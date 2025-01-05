d94439a7be7e3f654a30901c9f2def12876df1d4
gunyu1019 <gunyu1019@gmail.com>
2025-01-05 11:16:11 +0000

    [Feat] More simply weekday object.

--- utils/weekday.py
+++ utils/weekday.py
@@ -1,18 +1,22 @@
 import datetime
 
-from typing import NamedTuple
 
+class Weekday:
+    def __init__(self, date: datetime.date):
+        self.date = date
 
-class WeekDayResponse(NamedTuple):
-    Monday: datetime.date
-    Friday: datetime.date
-    Saturday: datetime.date
-    Sunday: datetime.date
+    @property
+    def monday() -> datetime.date:
+        return self.date + datetime.timedelta(days=-self.date.weekday())
 
+    @property
+    def friday() -> datetime.date:
+        return self.monday + datetime.timedelta(days=4)
 
-def weekday(date: datetime.date) -> WeekDayResponse:
-    monday_date = date + datetime.timedelta(days=-date.weekday())
-    friday_date = monday_date + datetime.timedelta(days=4)
-    saturday_date = monday_date + datetime.timedelta(days=5)
-    sunday_date = monday_date + datetime.timedelta(days=6)
-    return WeekDayResponse(monday_date, friday_date, saturday_date, sunday_date)
+    @property
+    def saturday() -> datetime.date:
+        return self.monday + datetime.timedelta(days=5)
+
+    @property
+    def sunday() -> datetime.date:
+        return self.monday + datetime.timedelta(days=6)
