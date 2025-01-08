4bdace4690e92fc1e565242f23178bee2a63d933
gunyu1019 <gunyu1019@gmail.com>
2025-01-08 11:08:17 +0000

    [Feat] (WIP-lab) Add dormitory client at router.get_meal

--- routers/get_meal.py
+++ routers/get_meal.py
@@ -5,10 +5,14 @@ from sqlalchemy.ext.asyncio import async_sessionmaker
 
 from models.building import Building
 from models.endpoint import MealInfo as MealInfoEndpoint
+from modules.dormitory_meal import DormitoryMeal
+from modules.school_meal import SchoolMeal
 from repository.meal_repository import MealRepository
 
 router = APIRouter()
 database = MealRepository()
+
+dormitory_client = DormitoryMeal(loop=router.lifespan)
 
 
 @router.get("/", response_model=list[MealInfoEndpoint])
@@ -26,6 +30,15 @@ async def get_meal(
     else:
         _building_for_fliter = list(Building)
 
+    for building in _building_for_fliter:
+        exist = await session.meal_exist(building, date)
+        if exist:
+            continue
+            
+        if building.type == "dormitory":
+            pass
+        else:
+            pass
     result = await session.meal(_building_for_fliter, date)
     return [await MealInfoEndpoint.model_validate_sql(x) for x in result]
 
